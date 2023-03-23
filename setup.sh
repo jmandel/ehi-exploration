3c1d81461cb66b8251d65061361eb336acfe5321
Josh Mandel <jmandel@alum.mit.edu>
2023-03-22 22:08:41 -0500

    Progress output in setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,25 @@
 
 wget https://open.epic.com/EHITables/Download -O tables.zip
 unzip tables.zip
-mkdir output
+mkdir -p output
 
-find . -name "*.htm" | xargs -I {} -P "$(nproc)" sh -c "deno run --allow-read extract_json.ts \"{}\" >   output/\$(basename \"{}\" .htm).json"
+# Run the find command in the background and process files in parallel
+find . -name "*.htm" | xargs -I {} -P "$(nproc)" sh -c "deno run --allow-read extract_json.ts \"{}\" > output/\$(basename \"{}\" .htm).json" &
+
+# Get the process ID of the background find command
+find_pid=$!
+
+# Count the total number of files in output every 5 seconds
+while true; do
+  sleep 10
+  file_count=$(find output -type f | wc -l)
+  echo "Files in output: $file_count"
+
+  # Check if the find command has finished running
+  if ! kill -0 "$find_pid" 2> /dev/null; then
+    break
+  fi
+done
+
+# Final count of files in output
+echo "Total files in output: $(find output -type f | wc -l)"
